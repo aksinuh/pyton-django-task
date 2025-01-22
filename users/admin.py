e693e6490a7bfa23683bb692737c7d5df6e4d528
aksinuh <nezirovaksin41@gmail.com>
2025-01-22 22:57:28 +0400

    users

---
+++ users/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.contrib.auth import get_user_model
+# Register your models here.
+
+User = get_user_model()
+
+@admin.register(User)
+class Useradmin(admin.ModelAdmin):
+    list_display = ["id", "username", "email"]
+    list_display_links = ["username"]
